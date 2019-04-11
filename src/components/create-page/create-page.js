05968afad326a4286bcdea81a40083f759c04e0b
Stephanie Beres <stephanie.beres34@gmail.com>
2019-04-10 21:32:54 -0600

    create-page added

---
+++ src/components/create-page/create-page.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {Link, Redirect} from 'react-router-dom';
+
+import createPage from './create-page.css';
+
+export function createPage(props) {
+    // If we are logged in redirect straight to the user's dashboard
+    if (props.loggedIn) {
+        return <Redirect to="/createPage" />;
+    }
+
+    return (
+        <header> 
+        <h1>Tripvy</h1>
+        </header>
+        <div class="create-page">
+      
+        <form id="list">
+            <p>Where are you going?</p><br>
+            <input type="text" name="location" id="location">
+            <br>
+            <br>
+            <p>What do you need?</p><br>
+            <input type="text" name="item" id="item">
+            <br>
+            <input type="text" name="item" id="item">
+            <br>
+            <input type="text" name="item" id="item">
+            <br>
+            <button id="add">Add Field</button>
+        </form>
+        </div>
+        <div class="footer">
+            <p>Stephanie Beres</p>
+        </div>
+     
+    );
+}
+
+
+const mapStateToProps = state => ({
+    loggedIn: state.auth.currentUser !== null
+});
+
+export default connect(mapStateToProps)(createPage);
