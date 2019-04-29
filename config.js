04a1ec18cad23c7d2b5a70885619c460211c15cc
Stephanie Beres <stephanie.beres34@gmail.com>
2019-04-29 15:11:35 -0500

    landing images

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    PORT: process.env.PORT || 8080,
-    // other stuff
+    PORT: process.env.PORT || 3000,
+    
     API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-      "http://localhost:3000/api"
+      "http://localhost:8080/api"
   };
